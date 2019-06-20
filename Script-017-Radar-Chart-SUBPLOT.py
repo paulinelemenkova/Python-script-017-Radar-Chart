45e6dc376db673902f39aeae37f64b024a2d2bb3
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-20 09:12:08 +0800

    Add files via upload

--- Script-017-Radar-Chart-SUBPLOT.py
+++ Script-017-Radar-Chart-SUBPLOT.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[11]:
-
-
-#!/usr/bin/env python
-# coding: utf-8
-#
-import matplotlib.pyplot as plt
+import os
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from math import pi
 import seaborn as sns
-import numpy as np
-import os
-#
+
 os.chdir('/Users/pauline/Documents/Python')
 df = pd.read_csv("Tab-Morph.csv")
-#
+
 # show 6 different variables on our radar chart, so take them out and set as a np.array.
 labels=np.array(['Median', 'Max', '1stQ', '3rdQ', 'Min', 'Mean'])
 stats1=df.loc[1, labels].values
@@ -25,7 +18,7 @@ stats3=df.loc[12, labels].values
 stats4=df.loc[15, labels].values
 stats5=df.loc[18, labels].values
 stats6=df.loc[23, labels].values
-#
+
 # close the plot
 angles=np.linspace(0, 2*np.pi, len(labels), endpoint=False)
 angles=np.concatenate((angles,[angles[0]]))
@@ -36,11 +29,10 @@ stats4=np.concatenate((stats4,[stats4[0]]))
 stats5=np.concatenate((stats5,[stats5[0]]))
 stats6=np.concatenate((stats6,[stats6[0]]))
 
-#  
 fig = plt.figure(figsize=(10.0, 5.0), dpi=300)
 fig.suptitle('Radar chart for the bathymetry of the Mariana Trench',
             fontsize=12, fontweight='bold', x=0.5, y=0.99)
-#
+
 # subplot 1
 ax = fig.add_subplot(231, polar=True)
 ax.plot(angles, stats1, 'o-', linewidth=2)
@@ -51,7 +43,7 @@ plt.setp(ax.get_xticklabels(), fontsize=10)
 plt.setp(ax.get_yticklabels(), fontsize=8)
 ax.grid(True)
 ax.annotate('(A)', fontsize=14, xy=(1.02, .90), xycoords="axes fraction")
-#
+
 # subplot 2
 ax = fig.add_subplot(232, polar=True)
 ax.plot(angles, stats2, 'o-', linewidth=2)
@@ -62,7 +54,7 @@ plt.setp(ax.get_xticklabels(), fontsize=10)
 plt.setp(ax.get_yticklabels(), fontsize=8)
 ax.grid(True)
 ax.annotate('(D)', fontsize=14, xy=(1.02, .90), xycoords="axes fraction")
-#
+
 # subplot 3
 ax = fig.add_subplot(233, polar=True)
 ax.plot(angles, stats3, 'o-', linewidth=2)
@@ -73,7 +65,7 @@ plt.setp(ax.get_xticklabels(), fontsize=10)
 plt.setp(ax.get_yticklabels(), fontsize=8)
 ax.grid(True)
 ax.annotate('(B)', fontsize=14, xy=(1.02, .90), xycoords="axes fraction")
-#
+
 # subplot 4
 ax = fig.add_subplot(234, polar=True)
 ax.plot(angles, stats4, 'o-', linewidth=2)
@@ -84,7 +76,7 @@ plt.setp(ax.get_xticklabels(), fontsize=10)
 plt.setp(ax.get_yticklabels(), fontsize=8)
 ax.grid(True)
 ax.annotate('(E)', fontsize=14, xy=(1.02, .90), xycoords="axes fraction")
-#
+
 # subplot 5
 ax = fig.add_subplot(235, polar=True)
 ax.plot(angles, stats5, 'o-', linewidth=2)
@@ -95,7 +87,7 @@ plt.setp(ax.get_xticklabels(), fontsize=10)
 plt.setp(ax.get_yticklabels(), fontsize=8)
 ax.grid(True)
 ax.annotate('(C)', fontsize=14, xy=(1.02, .90), xycoords="axes fraction")
-#
+
 # subplot 6
 ax = fig.add_subplot(236, polar=True)
 ax.plot(angles, stats6, 'o-', linewidth=2)
@@ -106,14 +98,7 @@ plt.setp(ax.get_xticklabels(), fontsize=10)
 plt.setp(ax.get_yticklabels(), fontsize=8)
 ax.grid(True)
 ax.annotate('(F)', fontsize=14, xy=(1.02, .90), xycoords="axes fraction")
-#
+
 plt.tight_layout()
 fig.savefig('plot_RadarChart.png', dpi=300)
 plt.show()
-
-
-# In[ ]:
-
-
-
-
